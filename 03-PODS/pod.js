661a287f67a48fb3ef90ca504b97a3fe465a9596
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-04-11 00:24:19 +0100

    logging pods

--- 03-PODS/pod.js
+++ 03-PODS/pod.js
@@ -23,8 +23,34 @@ kubectl port-forward pod/personal 8080:80
 
 // POD MANAGEMENT COMMANDS
 
+kubectl get pods -w
+
 // this will delete a pod 
 kubectl delete pod personal
 
+
 // this will run a pod using declarative command
-kubectl apply -f pod.yml
+kubectl apply - f pod.yml 
+
+// this will get everything in all namespaces
+kubectl get all 
+kubectl get all - A 
+
+// this will retrieve pods in one particular namespace 
+kubectl get pod - n kube - system 
+
+// this will retrieve all namespaces
+kubectl get namspaces 
+
+// this will give information about a specific pod
+kubectl describe pod personal 
+
+// this will output your pod in yaml
+kubectl get pod personal -o yaml
+kubectl get pod personal -o json 
+
+//this will get the logs for specific pod
+kubectl logs personal
+
+//this will get logs for a specific container within a pod 
+kubectl logs personal -c personal
