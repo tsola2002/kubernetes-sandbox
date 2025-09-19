6f3f02869e54e4203a9ba2c6fd21d65a66516ac4
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-09-19 10:11:04 +0100

    adding a secret

---
+++ 09-SECRETS/secret.js
@@ -0,0 +1,7 @@
+// STEP 1 IMPERATIVELY CREATE A SECRET
+kubectl create secret generic mysecret--from-literal=db - password=123
+--from-literal=api - token=token
+
+
+//STEP 2 RETRIEVE THE SECRET YOU JUST CREATED 
+kubectl get secret
