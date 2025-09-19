14597a8dac246d31d760e45664bfe6307d38ff46
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-09-19 10:12:14 +0100

    secret

--- 09-SECRETS/secret.js
+++ 09-SECRETS/secret.js
@@ -1,6 +1,6 @@
 // STEP 1 IMPERATIVELY CREATE A SECRET
 kubectl create secret generic mysecret--from-literal=db - password=123
---from-literal=api - token=token
+--from-literal=api-token=token
 
 
 //STEP 2 RETRIEVE THE SECRET YOU JUST CREATED 
