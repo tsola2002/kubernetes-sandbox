5cb725b3d19f90fc0cbe8609daf6a037e15577a7
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-09-19 13:21:17 +0100

    secret

--- 09-SECRETS/secret.js
+++ 09-SECRETS/secret.js
@@ -5,3 +5,16 @@ kubectl create secret generic mysecret--from-literal=db - password=123
 
 //STEP 2 RETRIEVE THE SECRET YOU JUST CREATED 
 kubectl get secret
+kubectl get secret mysecret - o yaml
+kubectl describe secret mysecret
+
+// STEP 3 CREATE A SECRET FROM A FILE
+// CREATE A FILE CALLED SECRET BEFORE RUNNING THE COMMAND 
+kubectl create secret generic mysecret - from - file --from-file=secret
+kubectl get secret mysecret - from - file - o yaml
+
+
+//STEP 4 DELETE A SECRET
+rm secret
+
+// STEP 5 CONSUME A SECRET FROM A POD
