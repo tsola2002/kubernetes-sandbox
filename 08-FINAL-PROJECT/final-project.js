92b35e2c82b7e7e40913285851a9b5295da7e1da
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-09-30 12:06:40 +0100

    kubernetes

--- 08-FINAL-PROJECT/final-project.js
+++ 08-FINAL-PROJECT/final-project.js
@@ -3,9 +3,19 @@ localhost:8080/swagger-ui/index.html
 
 
 // STEP 2 GENERATE DOCKER IMAGES FOR BOTH CUSTOMER AND ORDER MICROSERVICE
-docker build image -t customer-java-app . 
-docker build image - t order - java - app.
+docker build -t customer-java-app . 
+docker build  -t order-java-app.
 docker image ls
 
 
 // STEP 3 RUN BOTH IMAGES AS DOCKER CONTAINERS
+docker run --name customer-java-app  -d -p 8080:8080 customer-java-app
+docker run --name order-java-app  -d -p 8080:8080 order-java-app
+
+// STEP 4 CREATE 2 REPOSITORIES FOR CUSTOMER AND ORDER MICROSERVICE 
+// LINK THE REPOSITORIES PUSH THE IMAGES TO DOCKER HUB
+docker tag customer-java-app:latest tsola2002/customer-java-app:latest
+docker tag order - java - app:latest tsola2002 / order - java - app: latest 
+
+docker push tsola2002/customer-java-app:latest
+docker push tsola2002/customer-java-app:latest
